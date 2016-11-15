487414398b9416800675b02bec88f67dbd74a063
Julia Troxell <jst2152@columbia.edu>
2016-11-14 23:14:28 -0500

    working photo upload through local folder static

--- ss-ase/app/feed_module/controllers.py
+++ ss-ase/app/feed_module/controllers.py
@@ -1,7 +1,12 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, send_from_directory
 from .models import Listing
+import os
 
 feed_module = Blueprint('start', __name__, url_prefix='/')
+
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+target = os.path.join(APP_ROOT, "../static")
+ALLOWED_EXTENSIONS = set(['jpg'])
 
 @feed_module.route("")
 def welcome():
@@ -10,7 +15,7 @@ def welcome():
 @feed_module.route("feed")
 def feed():
 	listings = Listing.objects()
-	return render_template("feed.html", listings=listings)
+	return render_template("feed.html", listings=listings, display=send_image)
 
 @feed_module.route("list", methods=["GET", "POST"])
 def list():
@@ -20,7 +25,12 @@ def list():
 		size = request.form["list_size"]
 		price = request.form["list_price"]
 		info = request.form["list_info"]
-		if not title or not size or not price:
+		photo = request.files['file']
+		filename = photo.filename
+
+		image_names = os.listdir(target)
+
+		if not title or not size or not price or (filename == ""):
 			error = 1 #required fields are not filled
 			return render_template("listing.html", error=error)
 		elif not isNumber(price):
@@ -29,8 +39,18 @@ def list():
 		elif Listing.objects(title=title):
 			error = 3 #title is not unique
 			return render_template("listing.html", error=error, title=title)
+		elif not allowed_file(filename):
+			error = 4 #not a .jpg file
+			return render_template("listing.html", error=error, title=title)
+		elif photo.filename in image_names:
+			error = 5 #jpg is not unique
+			return render_template("listing.html", error=error, title=title)
+		
 		else: 
-			new_list = Listing(title, size, price, info)
+			destination = "/".join([target, filename])
+			photo.save(destination)
+
+			new_list = Listing(title, size, price, info, filename)
 			new_list.save()
 			return render_template("confirm.html", title=title)
 	else:
@@ -40,7 +60,11 @@ def list():
 @feed_module.route("listing/<title>")
 def listing(title):
 	item = Listing.objects(title=title)
-	return render_template("see_listing.html", title=title, size=item[0].size, price=item[0].price, info=item[0].info)
+	return render_template("see_listing.html", item=item[0])
+
+@feed_module.route("images/<filename>")
+def send_image(filename):
+	return send_from_directory("images", filename)
 
 def isNumber(price):
 	try:
@@ -48,5 +72,8 @@ def isNumber(price):
 		return True
 	except ValueError:
 		return False
+
+def allowed_file(filename):
+	return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
 	
--- ss-ase/app/feed_module/models.py
+++ ss-ase/app/feed_module/models.py
@@ -5,5 +5,6 @@ class Listing(db.Document):
 	size = db.StringField(required=True)
 	price = db.StringField(required=True)
 	info = db.StringField(required=True)
+	filename = db.StringField(required=True)
 	def get_title(self):
 		return self.title
