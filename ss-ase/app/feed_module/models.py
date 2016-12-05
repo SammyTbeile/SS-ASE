8194bbc9f6fa67025c9dd62cf10b1da58a7f3074
Sammy Tbeile <sammy.tbeile@columbia.edu>
2016-12-05 01:26:30 -0500

    Most pylint errors fixed

--- ss-ase/app/feed_module/models.py
+++ ss-ase/app/feed_module/models.py
@@ -1,10 +1,10 @@
 from app import db
 
 class Listing(db.Document):
-	title = db.StringField(required=True, unique=True)
-	size = db.StringField(required=True)
-	price = db.StringField(required=True)
-	info = db.StringField(required=False)
-	filename = db.StringField(required=True)
-	def get_title(self):
-		return self.title
+    title = db.StringField(required=True, unique=True)
+    size = db.StringField(required=True)
+    price = db.StringField(required=True)
+    info = db.StringField(required=False)
+    filename = db.StringField(required=True)
+    def get_title(self):
+        return self.title
